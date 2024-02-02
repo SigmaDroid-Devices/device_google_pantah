0b6bc2d3271e7698a56acfdc519a0aa9e9e6fda9
LurdRaiden <everethduncan@gmail.com>
2024-02-01 21:37:37 -0500

    include face unlock
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -27,6 +27,9 @@ include hardware/google/pixel/powershare/device.mk
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
 
+# Face Unlock
+include vendor/google/faceunlock/device.mk
+
 # Gapps
 WITH_GMS := true
 WITH_GAPPS := true
