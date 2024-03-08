712b40d3a18a9bd8b91555daa29fc115d63ec17e
LurdRaiden <everethduncan@gmail.com>
2024-03-07 16:01:46 -0800

    pantah: add SigmaDroid specifc build flags
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -27,6 +27,20 @@ include hardware/google/pixel/powershare/device.mk
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
 
+# Gapps
+WITH_GMS := true
+WITH_GAPPS := true
+BUILD_GMS_OVERLAYS_AND_PROPS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+# Sigma build flags
+SIGMA_BUILD_TYPE := OFFICIAL
+SIGMA_MAINTAINER := LurdRaiden
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+
 # Build necessary packages for vendor
 
 # Audio
